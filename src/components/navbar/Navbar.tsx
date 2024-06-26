e9993279275b0f23c484afcbffd3dc8b19d5389f
Evandro Calado <evandrocalado07@gmail.com>
2024-06-26 12:42:19 -0300

    🦄 refactor: hover

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,18 +1,21 @@
 import { links } from '@/utils';
 import Link from 'next/link';
+import { Separator } from '../ui/separator';
 
 export const Navbar = () => {
   return (
     <nav>
       <ul className="hidden items-center gap-4 md:flex">
         {links.map((link) => (
-          <li key={link.href}>
+          <li key={link.href} className="group">
             <Link
               href={link.href}
-              className="relative font-light capitalize after:absolute after:-bottom-1 after:left-1/2 after:h-[1px] after:w-0 after:-translate-x-1/2 after:bg-primary after:duration-150 after:content-[''] hover:text-primary after:hover:w-full"
+              className="group relative font-light capitalize duration-150 hover:text-primary"
             >
               {link.label}
             </Link>
+
+            <Separator className="mx-auto mt-1 w-0 duration-150 group-hover:w-full group-hover:bg-gradient-to-r group-hover:from-primary-foreground group-hover:via-primary group-hover:to-primary-foreground" />
           </li>
         ))}
       </ul>
