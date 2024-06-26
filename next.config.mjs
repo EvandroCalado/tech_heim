603c2098f6225ab24c84d5f495b9815d1c01d3ec
Evandro Calado <evandrocalado07@gmail.com>
2024-06-26 16:34:03 -0300

    🐳 chore: add remote pattern cloudinary

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'i.dell.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
     ],
   },
 };
