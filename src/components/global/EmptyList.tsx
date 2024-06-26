c1ff766d8f733a150a8b94cd34307bc5d72890a1
Evandro Calado <evandrocalado07@gmail.com>
2024-06-25 21:23:38 -0300

    ✨ feat: create empty list

--- src/components/global/EmptyList.tsx
+++ src/components/global/EmptyList.tsx
@@ -1,11 +1,14 @@
+import { cn } from '@/lib/utils';
 import { FC } from 'react';
 
-export interface EmptyListProps {}
+export interface EmptyListProps {
+  heading?: string;
+  className?: string;
+}
 
-export const EmptyList: FC<EmptyListProps> = () => {
-  return (
-    <div>
-      <h1>EmptyList</h1>
-    </div>
-  );
+export const EmptyList: FC<EmptyListProps> = ({
+  heading = 'Nenhum resultado encontrado.',
+  className,
+}) => {
+  return <h2 className={cn('text-xl', className)}>{heading}</h2>;
 };
