9b075fb9a9c94b1effaedc5978eb7dcfcc4fbbd1
Evandro Calado <evandrocalado07@gmail.com>
2024-06-26 16:34:26 -0300

    🐳 chore: create hero links

---
+++ src/utils/heroLinks.ts
@@ -0,0 +1,17 @@
+export const heroLinks = [
+  {
+    id: 1,
+    image:
+      'https://res.cloudinary.com/dx2lwce8m/image/upload/v1719427850/xwno91s9p37rtm1xh7xc.png',
+  },
+  {
+    id: 2,
+    image:
+      'https://res.cloudinary.com/dx2lwce8m/image/upload/v1719427850/pqepog5h7qks09f1xm0i.png',
+  },
+  {
+    id: 3,
+    image:
+      'https://res.cloudinary.com/dx2lwce8m/image/upload/v1719427850/je8mdxzpn231v4zft1r7.png',
+  },
+];
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
 export * from './Links';
 export * from './db';
 export * from './formatCurrency';
+export * from './heroLinks';
