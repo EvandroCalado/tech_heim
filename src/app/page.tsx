bd160a1797299bb9927bf30e6218082b3fd39560
Evandro Calado <evandrocalado07@gmail.com>
2024-06-26 13:59:49 -0300

    🐳 chore: add container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
+import { Container } from '@/components/global';
 import { FeaturedProducts, Hero } from '@/components/home';
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <FeaturedProducts />
+
+      <Container>
+        <FeaturedProducts />
+      </Container>
     </>
   );
 }
