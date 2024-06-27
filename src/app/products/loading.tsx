4902f55fbe741b259040465cd4d9fd34caf3969e
Evandro Calado <evandrocalado07@gmail.com>
2024-06-27 19:41:30 -0300

    ✨ feat: create loading products

---
+++ src/app/products/loading.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { Container, LoadingContainer } from '@/components/global';
+
+const Loading = () => {
+  return (
+    <Container className="my-20">
+      <LoadingContainer />
+    </Container>
+  );
+};
+
+export default Loading;
