de57539ea433959109ba9e7bccb61845568d4e7f
Evandro Calado <evandrocalado07@gmail.com>
2024-06-26 22:04:39 -0300

    ✨ feat: create loading container component

--- src/components/global/LoadingContainer.tsx
+++ src/components/global/LoadingContainer.tsx
@@ -1,11 +1,25 @@
-import { FC } from 'react';
+import { Card, CardContent } from '../ui/card';
+import { Skeleton } from '../ui/skeleton';
 
-export interface LoadingContainerProps {}
-
-export const LoadingContainer: FC<LoadingContainerProps> = () => {
+export const LoadingContainer = () => {
   return (
-    <div>
-      <h1>LoadingContainer</h1>
+    <div className="grid gap-4 pt-12 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      <LoadingProducts />
+      <LoadingProducts />
+      <LoadingProducts />
+      <LoadingProducts />
     </div>
+  );
+};
+
+const LoadingProducts = () => {
+  return (
+    <Card>
+      <CardContent className="p-4">
+        <Skeleton className="h-48 w-full" />
+        <Skeleton className="mt-4 h-4 w-3/4" />
+        <Skeleton className="mt-4 h-4 w-1/2" />
+      </CardContent>
+    </Card>
   );
 };
