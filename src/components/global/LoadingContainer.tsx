b89194c0b4d75a9bd6a802108f48263832d6c567
Evandro Calado <evandrocalado07@gmail.com>
2024-06-27 19:42:22 -0300

    🌈 style: change styles

--- src/components/global/LoadingContainer.tsx
+++ src/components/global/LoadingContainer.tsx
@@ -3,8 +3,7 @@ import { Skeleton } from '../ui/skeleton';
 
 export const LoadingContainer = () => {
   return (
-    <div className="grid gap-4 pt-12 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-      <LoadingProducts />
+    <div className="grid gap-4 pt-12 md:grid-cols-2 lg:grid-cols-3">
       <LoadingProducts />
       <LoadingProducts />
       <LoadingProducts />
@@ -14,9 +13,10 @@ export const LoadingContainer = () => {
 
 const LoadingProducts = () => {
   return (
-    <Card>
+    <Card className="border-none shadow-none">
       <CardContent className="p-4">
         <Skeleton className="h-48 w-full" />
+        <Skeleton className="mt-4 h-2 w-full" />
         <Skeleton className="mt-4 h-4 w-3/4" />
         <Skeleton className="mt-4 h-4 w-1/2" />
       </CardContent>
