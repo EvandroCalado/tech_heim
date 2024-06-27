4576a7dae2561eb8f71ff16d74c398c005c42104
Evandro Calado <evandrocalado07@gmail.com>
2024-06-27 18:38:34 -0300

    ✨ feat: create products container

--- src/components/products/ProductsContainer.tsx
+++ src/components/products/ProductsContainer.tsx
@@ -1,11 +1,113 @@
+import { fetchAllProducts } from '@/actions';
+import { IconGrid, IconList } from '@/icons';
+import Link from 'next/link';
 import { FC } from 'react';
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from '../ui/breadcrumb';
+import { Button } from '../ui/button';
+import { Separator } from '../ui/separator';
+import { ProductsFilters } from './ProductsFilters';
+import { ProductsGrid } from './ProductsGrid';
+import { ProductsList } from './ProductsList';
 
-export interface ProductsContainerProps {}
+export interface ProductsContainerProps {
+  layout?: 'list' | 'grid';
+  brand?: string;
+  search?: string;
+}
 
-export const ProductsContainer: FC<ProductsContainerProps> = () => {
+export const ProductsContainer: FC<ProductsContainerProps> = async ({
+  layout,
+  brand,
+  search,
+}) => {
+  const products = await fetchAllProducts();
+
+  const totalProducts = products.length;
+  const searchQuery = search ? `&search=${search}` : '';
+
   return (
-    <div>
-      <h1>ProductsContainer</h1>
-    </div>
+    <>
+      {/* BREADCRUMB */}
+      <Breadcrumb className="mb-4">
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <Link href="/" className="hover:text-primary/90 hover:underline">
+              Home
+            </Link>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              <Link
+                href={`/products?layout=${layout}${searchQuery}`}
+                className="hover:text-primary/90 hover:underline"
+              >
+                Produtos
+              </Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+
+      {/* HEADER */}
+      <div>
+        <div className="flex items-center justify-between">
+          <h4 className="text-lg font-medium">
+            {totalProducts} produto{totalProducts > 1 && 's'}
+          </h4>
+
+          <div className="flex gap-x-4">
+            <Button
+              variant={layout === 'grid' ? 'default' : 'ghost'}
+              size="icon"
+            >
+              <Link href={`/products?layout=grid${searchQuery}`}>
+                <IconGrid
+                  className={`${layout === 'grid' ? 'text-primary-foreground' : 'text-secondary-foreground'}`}
+                />
+              </Link>
+            </Button>
+            <Button
+              variant={layout === 'list' ? 'default' : 'ghost'}
+              size="icon"
+            >
+              <Link href={`/products?layout=list${searchQuery}`}>
+                <IconList
+                  className={`${layout === 'list' ? 'text-primary-foreground' : 'text-secondary-foreground'}`}
+                />
+              </Link>
+            </Button>
+          </div>
+        </div>
+      </div>
+
+      <Separator className="mt-4" />
+
+      {/* FILTERS */}
+      <div className="flex gap-4">
+        <div className="flex-1">
+          <ProductsFilters layout={layout} search={search} brand={brand} />
+        </div>
+
+        {/* PRODUCTS */}
+        <div className="flex-[4]">
+          {totalProducts === 0 ? (
+            <h5 className="mt-16 text-2xl">
+              Desculpe, nenhum produto encontrado
+            </h5>
+          ) : layout === 'grid' ? (
+            <ProductsGrid products={products} />
+          ) : (
+            <ProductsList products={products} />
+          )}
+        </div>
+      </div>
+    </>
   );
 };
