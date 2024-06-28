cad4de7dfb88e7b1a8505fceaba0acf2aeb2dbef
Evandro Calado <evandrocalado07@gmail.com>
2024-06-28 12:12:01 -0300

    🐳 chore: remove search stories

--- src/stories/header/Search.stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Search } from '@/components/header';
-import { Meta } from '@storybook/react';
-
-export default {
-  title: 'Components/Header/Search',
-  component: Search,
-} as Meta;
-
-export const Default = () => <Search />;
